462a0dfed6a9f1bfff97d9f1d16a7ca40e5a73ef
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2025-10-11 13:42:33 +0530

    Create _worker.js

---
+++ a/_worker.js
@@ -0,0 +1,24 @@
+export default {
+  async fetch(request,env) {
+    const url = new URL(request.url);
+    const pathParts = url.pathname.split("/")[1];
+    const LINKS = env.LINKS; // KV binding
+    if (url.pathname.endsWith('*')) {
+      // Remove the trailing *
+      url.pathname = url.pathname.slice(0, -1);
+  
+      // Redirect with 302
+      return Response.redirect(url.toString(), 302);
+    }
+    if (pathParts) {
+      const value = await LINKS.get(path);
+  if(value) {
+return Response.redirect(value, 302);
+  } else {
+return new Response("Not Found");
+  }
+    }
+
+  //  return Response.redirect("https://sharelinks.in/", 302);
+  }
+}
