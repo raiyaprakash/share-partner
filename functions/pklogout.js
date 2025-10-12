2310bccfd945ef15ade65f5fe5337979bed89075
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2025-10-12 22:19:58 +0530

    Update and rename logout.js to pklogout.js

---
+++ functions/pklogout.js
@@ -0,0 +1,10 @@
+export const onRequestGet = async () => {
+  const cookieOptions = "Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0";
+
+  const headers = new Headers();
+  headers.append("Set-Cookie", `referid=; ${cookieOptions}`);
+  headers.append("Set-Cookie", `partner_name=; ${cookieOptions}`);
+  headers.append("Location", "/login");
+
+  return new Response(null, { status: 302, headers });
+};
