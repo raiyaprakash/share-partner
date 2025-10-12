c5e847f2996d7b1df36e6cd8d903877d39de8f3e
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2025-10-12 22:13:14 +0530

    Create logout.js

---
+++ functions/logout.js
@@ -0,0 +1,13 @@
+export const onRequestPost = async ({ request, env }) => {
+  const cookieOptions = "Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0";
+
+  const headers = new Headers();
+  headers.append("Set-Cookie", `referid=; ${cookieOptions}`);
+  headers.append("Set-Cookie", `partner_name=; ${cookieOptions}`);
+  headers.append("Location", "/login");
+
+  return new Response(null, {
+    status: 302,
+    headers,
+  });
+};
