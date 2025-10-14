45c75431f1c251da2f709234a65a625cde1867e5
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2025-10-14 20:20:29 +0530

    Update logout.js

--- functions/logout.js
+++ functions/logout.js
@@ -4,6 +4,7 @@ export const onRequestGet = async () => {
   const headers = new Headers();
   headers.append("Set-Cookie", `referid=; ${cookieOptions}`);
   headers.append("Set-Cookie", `partner_name=; ${cookieOptions}`);
+  headers.append("Set-Cookie", `partner_pass=; ${cookieOptions}`);
   headers.append("Location", "/login");
 
   return new Response(null, { status: 302, headers });
