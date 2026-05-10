a9ac3848a53f10216ec5c69eef02b702b72c57aa
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2026-05-10 08:49:32 +0530

    Update payment-history.js

--- functions/payment-history.js
+++ functions/payment-history.js
@@ -151,7 +151,7 @@ export const onRequest = async ({ request, env }) => {
                     (w) => `
                     <tr class="hover:bg-slate-50 transition">
 
-                      <td class=w-[30%] px-4 py-3">
+                      <td class="w-[30%] px-4 py-3">
 
                         <div class="inline-flex items-center gap-2 bg-green-50 text-green-700 px-3 py-1.5 rounded-lg text-sm font-bold">
 
